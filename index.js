0a87ee695f4e3219e5b6dfb3890e4a7e97957872
Quammy93 <Quammy93@gmail.com>
2023-10-21 22:02:06 +0000

    update

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress, language, software });
+});
+
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
